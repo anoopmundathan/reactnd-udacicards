a1ca6ab6134ea8d94c046505315676d48768afc7
Anoop Mundathan <anoop.mundathan@gmail.com>
2017-11-06 06:49:19 +0000

    Create Navigator component

--- App.js
+++ App.js
@@ -3,63 +3,8 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import { Text, View, ScrollView, StyleSheet, } from 'react-native'
-import { TabNavigator, StackNavigator } from 'react-navigation'
-import DeckList from './components/DeckList'
-import DeckDetails from './components/DeckDetails'
-import AddCard from './components/AddCard'
-import AddDeck from './components/AddDeck'
-import Quiz from './components/Quiz'
-import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { setLocalNotification } from './utils/helpers'
-
-const Tabs = TabNavigator({
-  All: {
-    screen: DeckList,
-    navigationOptions: {
-      tabBarLabel: 'All Decks',
-      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-card' size={30} color={tintColor} />
-    }
-  },
-  Add: {
-    screen: AddDeck,
-    navigationOptions: {
-      tabBarLabel: 'Add Deck',
-      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-add-circle' size={30} color={tintColor} />
-    }
-  }
-})
-
-const DeckStack = StackNavigator({
-  DeckList: {
-    screen: Tabs,
-    navigationOptions: {
-      title: 'Decks'
-    }
-  },
-  DeckDetails: {
-    screen: DeckDetails,
-    navigationOptions: ({ navigation }) => ({
-      headerTintColor: 'white',
-      title: `${navigation.state.params.name}`,
-      headerStyle: {
-        backgroundColor: 'purple',
-      }
-    })
-  },
-  AddCard: {
-    screen: AddCard,
-    navigationOptions: ({ navigation }) => ({
-      headerTintColor: 'white',
-      title: `${navigation.state.params.name}`,
-      headerStyle: {
-        backgroundColor: 'purple',
-      }
-    })
-  },
-  Quiz: {
-    screen: Quiz
-  }
-})
+import { DeckStack } from './components/Navigator'
 
 export default class App extends React.Component {
   componentDidMount() {
